849c45c56e11381f46189fcb8080c81cd12c9c46
alfonsov12-collab <alfonsov12@hotmail.com>
2026-07-07 21:58:39 +0200

    login 1

--- app/index.js
+++ app/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
   res.send('<h1>Trackium</h1><p>Welcome</p>');
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'login.html'));
 });
 
 app.listen(PORT, () => {
